5332c96fefa1548ef33f2af568ea6f76af6cb81e
Joepvg <35223909+joepvgenuchten@users.noreply.github.com>
2020-12-15 14:45:23 +0100

    created a separate config file and changed logo

---
+++ config.js
@@ -0,0 +1,41 @@
+var respecConfig = {
+  specStatus: "GN-WV",
+  specType: "ST",
+  //publishDate: "2020-03-23",
+  editors: [
+        { name: "Jan Bruinenberg", company: "Alliander", url: "https://www.alliander.com" },
+		{ name: "Ivo Kuijlaars",company: "Enexis", url: "https://www.enexis.nl" },
+		{ name: "Sefanja Severin",company: "Stedin", url: "https://www.stedin.net" },
+		{ name: "Frank Boere",company: "Enduris", url: "https://www.enduris.nl" },
+		{ name: "Richard Flierman",company: "TenneT", url: "https://www.tennet.eu" },
+		{ name: "George Argyropoulos", company: "Stedin", url: "https://www.stedin.net" },
+		{ name: "Joep van Genuchten", company: "Alliander", url: "https://www.alliander.com" },
+      ],
+
+  github: "https://github.com/Alliander/DataStandaardenNetbeheerdersVIVET",
+  issueBase: "https://github.com/Alliander/DataStandaardenNetbeheerdersVIVET/issues/",
+  maxTocLevel: 3,
+  logos: [
+    {
+      src: "https://www.netbeheernederland.nl/_upload/News/Logo_Netbeheer_Nederland_RGB-1244.jpg?mode=crop&h=768&scale=both&w=1600",
+      url: "www.https://www.netbeheernederland.nl/",
+      alt: "Netbeheer Nederland",
+      width: 200,
+      height: 140,
+      id: "nbn-logo",
+    },
+  ],
+  //localBiblio: {
+  //  "ISO19136": {
+  //    "href": "https://www.iso.org/standard/32554.html",
+  //    "title": "ISO 19136:2007 Geographic information -- Geography Markup Language (GML)",
+  //    "authors": [""],
+  //    "date": "September 2007",
+  //    "publisher": "International Organization for Standardization",
+  //  },
+  //},
+  localBiblio: {
+
+  }
+  
+};
